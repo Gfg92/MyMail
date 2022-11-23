515c84d8cd4516c4b6f468d4824dc76d08b4234d
alu10423712 <alufer3712@ieselcaminas.org>
2022-11-23 19:06:31 +0100

    Initial commit

--- app/src/main/java/com/example/mymail/MainActivity.kt
+++ app/src/main/java/com/example/mymail/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.mymail
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.*
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,6 +14,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.example.mymail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -42,6 +46,26 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //Imagen redondo
+        val header = navView.getHeaderView(0)
+        val fotouser = header.findViewById<ImageView>(R.id.imageView)
+        val bm = BitmapFactory.decodeResource(applicationContext.resources, R.mipmap.fotocuadrada)
+        val drawable = RoundedBitmapDrawableFactory.create(applicationContext.resources, bm)
+        drawable.isCircular = true
+        fotouser.setImageDrawable(drawable)
+
+        //Spinner
+        val emails = arrayOf(
+            getString(R.string.nav_header_email),
+            getString(R.string.nav_header_email1),
+            getString(R.string.nav_header_email2)
+        )
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, emails)
+        val list_email = header.findViewById<Spinner>(R.id.spinner)
+        list_email.adapter = adaptador
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
